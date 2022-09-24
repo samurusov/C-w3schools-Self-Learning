f8d3099c6d2a1d8716e7a80895fc2214c899c43a
ismail ulusoy <samuelulusoy@gmail.com>
2022-09-24 16:14:17 +0300

    Added C-Fresh-Start and C-Empty folders

---
+++ C-Fresh-Start/Variables/format-specifiers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	mynum;
+
+	mynum = 14;
+	int	mynum2;
+
+	mynum2 = 16;
+	int	mytotal;
+
+	mytotal = mynum + mynum2;
+	char	mychar;
+
+	mychar = 'F';
+	printf("My favourite total is %i and my favourite character is %c\n", mytotal, mychar);
+	return (0);
+}
---
+++ C-Fresh-Start/Variables/multiple-variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int	x, y, z;
+	x = 14;
+	y = 13;
+	z = 3;
+	printf("%i\n", x + y + z);
+	return (0);
+}
